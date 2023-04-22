e5de3b42a30f5e3c1558ce00a1511f9f08cea481
lspongebobjh <lspongebobjh@gmail.com>
2023-04-22 13:10:40 +0000

    for test

--- run.sh
+++ run.sh
@@ -1,17 +1,17 @@
 #!/bin/bashexport 
 CUDA_VISIBLE_DEVICES=0
 
-python run_feat_extract.py \
-params.input_images="./data/CTC/Training/Fluo-N2DH-SIM+" \
-params.input_masks="./data/CTC/Training/Fluo-N2DH-SIM+" \
-params.input_seg="./data/CTC/Training/Fluo-N2DH-SIM+" \
-params.output_csv="./data/basic_features/" \
-params.sequences=['01','02'] \
-params.seg_dir='_GT/TRA' \
-params.basic=True
+# python run_feat_extract.py \
+# params.input_images="./data/CTC/Training/Fluo-N2DH-SIM+" \
+# params.input_masks="./data/CTC/Training/Fluo-N2DH-SIM+" \
+# params.input_seg="./data/CTC/Training/Fluo-N2DH-SIM+" \
+# params.output_csv="./data/basic_features/" \
+# params.sequences=['01','02'] \
+# params.seg_dir='_GT/TRA' \
+# params.basic=True
 
-# python run_train_metric_learning.py \
-# dataset.kwargs.data_dir_img="./data/CTC/Training/Fluo-N2DH-SIM+" \
-# dataset.kwargs.data_dir_mask="./data/CTC/Training/Fluo-N2DH-SIM+" \
-# dataset.kwargs.dir_csv="./data/basic_features/Fluo-N2DH-SIM+" \
-# dataset.kwargs.subdir_mask='GT/TRA'
+python run_train_metric_learning.py \
+dataset.kwargs.data_dir_img="./data/CTC/Training/Fluo-N2DH-SIM+" \
+dataset.kwargs.data_dir_mask="./data/CTC/Training/Fluo-N2DH-SIM+" \
+dataset.kwargs.dir_csv="./data/basic_features/Fluo-N2DH-SIM+" \
+dataset.kwargs.subdir_mask='GT/TRA'
